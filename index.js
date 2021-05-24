f95640de60679573b9da8d279e17a3ade47f4dc4
AlexShold <ashold12@gmail.com>
2021-05-24 17:09:34 -0500

    button click working

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ let browser = puppeteer.launch({headless: false}) //remove headless on final
       isMobile: false,
     });
 
-    page.goto("http://linkedin.com", {
-      waitUntil: "networkidle2",
-    });
+    page.goto("http://linkedin.com", {waitUntil: "networkidle2"});
 
     await page.waitFor('input[name="session_key"]')
     await page.waitFor(1000)
@@ -25,7 +23,7 @@ let browser = puppeteer.launch({headless: false}) //remove headless on final
     });
     await page.click('button[type="submit"]');
     await page.waitFor(1000)
-    const url = await page.url()
+    let url = await page.url()
     if (
       url === 'https://www.linkedin.com/uas/login-submit' ||
       url === 'https://www.linkedin.com/'
@@ -35,6 +33,19 @@ let browser = puppeteer.launch({headless: false}) //remove headless on final
         return
       }
     console.log('Successful Login')
+    list.forEach(async (userUrl) => {
+      page.goto(userUrl, { waitUntil: "networkidle2" });
+      if (page.url() === 'https://www.linkedin.com/in/unavailable/') return
+      await page.waitFor('button[data-control-name="skill_details"]')
+      await page.click('button[data-control-name="skill_details"]')
+      page.waitForNavigation();
+      await page.evaluate(() => {
+        let elements = document.getElementsByClassName('pv-skill-entity__featured-endorse-button-shared')
+        for (let x = 0; x < elements.length; x++) {
+          elements[x].click()
+        }
+      });
+    })
   })
   .catch((err) => {
     console.error(err)
